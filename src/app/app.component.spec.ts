0ef225549046755c7b60c4d5d171d5a0326f7ecc
Gandhi Aguirre <gandhi.aguirre@outlook.com>
2020-11-14 15:48:54 -0600

    Update unit testing for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ServerSideRenderAngularUniversal'`, () => {
+  it(`should have as title 'SSR Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ServerSideRenderAngularUniversal');
+    expect(app.title).toEqual('SSR Angular');
   });
 
-  it('should render title', () => {
+  it('should have 4 specific buttons Users,Home,Default,Location', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ServerSideRenderAngularUniversal app is running!');
+  
+    const buttons=compiled.querySelectorAll('button');
+    let buttonUsers= Array.from(buttons).find(button => button['textContent'] === "Users");
+    let buttonHome= Array.from(buttons).find(button => button['textContent'] === "Home");
+    let buttonDefault= Array.from(buttons).find(button => button['textContent'] === "Default");
+    let buttonLocation= Array.from(buttons).find(button => button['textContent'] === "Location");
+    
+    expect(!!buttonUsers&&!!buttonHome&&!!buttonDefault&& !!buttonLocation).toBeTrue();
   });
 });
