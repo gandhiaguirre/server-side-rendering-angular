237c38ff850e28a5cf0fa3d945dd530a9be3ef53
Gandhi Aguirre <gandhi.aguirre@outlook.com>
2020-11-14 14:47:39 -0600

    Fix navigator.geolocating, doing the action only on the client side

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PLATFORM_ID, Inject } from '@angular/core'
+import { isPlatformBrowser, isPlatformServer } from '@angular/common'
 @Component({
   selector: 'location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.sass']
 })
-export class LocationComponent implements OnInit{
-  
-  
-  
+export class LocationComponent implements OnInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId) { }
+
   ngOnInit(): void {
-      this.getLocation();
+    this.getLocation();
   }
   title = 'Location';
 
@@ -18,17 +19,20 @@ export class LocationComponent implements OnInit{
   position: Position;
 
   getLocation() {
-    console.log('getLocation');
-    
-    navigator.geolocation.getCurrentPosition((position: Position) => {
-      
-      this.position = position
-      console.log(position)
-    },
-      (errorCallback) => {
-        console.log(errorCallback);
-        
-        alert('Acceso denegado')
-      })
+    if (isPlatformBrowser(this.platformId)){
+      console.log('getLocation');
+
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+
+        this.position = position
+        console.log(position)
+      },
+        (errorCallback) => {
+          console.log(errorCallback);
+
+          alert('Acceso denegado')
+        })
+    }
   }
+
 }
